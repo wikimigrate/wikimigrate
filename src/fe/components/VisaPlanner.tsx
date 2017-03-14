3f91a4aa782f30e98132883e1108fa6fd12d879e
Andy Shu <andy@symbol.is>
2017-03-14 09:05:43 +0800

    Lay down style

--- src/fe/components/VisaPlanner.tsx
+++ src/fe/components/VisaPlanner.tsx
@@ -1,10 +1,20 @@
 import data from '../../data'
 import * as React from 'react'
 
+const style = {
+    fontSize: 14,
+    fontFamily: "sans-serif",
+    padding: "0.1em"
+}
+
 class VisaPlanner extends React.Component<{}, {}> {
 
     render() {
-        return <div> {JSON.stringify(data)} </div>;
+        return (
+            <div style={style}>
+                {JSON.stringify(data)}
+            </div>
+        );
     }
 
 }
