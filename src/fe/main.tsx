6fb61dd521da8a42f767506e27c0efa4032a666e
Andy Shu <andy@symbol.is>
2017-04-10 13:06:47 -0700

    Add todo

--- src/fe/main.tsx
+++ src/fe/main.tsx
@@ -7,6 +7,7 @@ import VisaPlanner from './components/VisaPlanner'
 
 const store = createStore(
     reducer,
+    // TODO: Remove under product mode
     (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
 )
 
