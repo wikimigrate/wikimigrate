c4c560f6c47c09ddb1270eed1f5b2b625667f07b
Andy Shu <andy@symbol.is>
2017-04-13 12:16:03 -0700

    Add official website data
    Signed-off-by: Andy Shu <andyshuhsin@gmail.com>

--- src/data/common/languageTestProfiles.ts
+++ src/data/common/languageTestProfiles.ts
@@ -5,37 +5,55 @@ const languageTestProfiles: LanguageTestProfile[] = [
         id: "clb",
         title: {
             en: "Canadian Language Benchmark"
-        }
+        },
+        reference: {
+            url: "http://www.language.ca/index.cfm?Repertoire_No=2137991327",
+        },
     },
     {
         id: "nclc",
         title: {
             fr: "Niveaux de compétence linguistique canadiens"
-        }
+        },
+        reference: {
+            url: "http://www.language.ca/index.cfm?Repertoire_No=2137991326",
+        },
     },
     {
         id: "ielts",
         title: {
             en: "International English Language Testing System"
+        },
+        reference: {
+            url: "https://www.ielts.org/",
         }
     },
     {
         id: "toefl",
         title: {
             en: "Test of English as a Foreign Language"
-        }
+        },
+        reference: {
+            url: "https://www.ets.org/toefl",
+        },
     },
     {
         id: "pte-academic",
         title: {
             en: "Pearson Test of English"
-        }
+        },
+        reference: {
+            url: "http://pearsonpte.com/",
+        },
     },
     {
         id: "cae",
         title: {
             en: "Cambridge English: Advanced"
-        }
+        },
+        reference: {
+            url: "http://www.cambridgeenglish.org/exams/advanced/",
+        },
     }
 ]
 
