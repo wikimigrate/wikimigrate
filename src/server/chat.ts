e2b76cbcf50ec9b8621aca5370d7368d97db9ab9
Andy Shu <andy@symbol.is>
2017-05-04 10:44:44 -0400

    Extract wechat middleware
    Signed-off-by: Andy Shu <andyshuhsin@gmail.com>

--- src/server/chat.ts
+++ src/server/chat.ts
@@ -1,70 +1,10 @@
 import * as Koa from "koa"
 import * as bodyParser from "koa-bodyparser"
 
-import {Context} from "koa"
-
-import {createHash} from "crypto"
-import {parseXml} from "./parseXml"
-
-
-interface WechatNormalTextData {
-    MsgType: "text"
-    ToUserName: string
-    FromUserName: string
-    CreateTime: string
-    Content: string
-    MsgId: string
-}
-
-const TOKEN = process.env["GOMIGRANT_WECHAT_TOKEN"]
+import {wechat} from "./middlewares/wechat"
 
 const chat = new Koa()
 const port = 20000
-
-type RequestType = "wechat" | "unknown"
-
-function getRequestType(url: string): RequestType {
-    if (url.includes("wechat")) {
-        return "wechat"
-    }
-    else {
-        return "unknown"
-    }
-}
-
-function sha1(s: any): string {
-    const hasher = createHash("sha1")
-    hasher.update(s)
-    return hasher.digest("hex").toString()
-}
-
-/**
- * @see https://mp.weixin.qq.com/wiki/8/f9a0b8382e0b77d87b3bcc1ce6fbc104.html
- */
-function isWechatSignatureValid(
-    token: string,
-    timestamp: string,
-    nonce: string,
-    signature: string,
-): boolean {
-    const hash = sha1([token, timestamp, nonce].sort().join(""))
-    return hash === signature
-}
-
-function getWechatVerificationResponseBody(query: Object): string {
-    const {
-        timestamp,
-        nonce,
-        signature,
-        echostr,
-    } = (query as any)
-    if (isWechatSignatureValid(TOKEN, timestamp, nonce, signature)) {
-        return echostr
-    }
-    else {
-        return ""
-    }
-}
 
 chat.use(bodyParser({
     enableTypes: ["text", "json", "form"],
@@ -72,34 +12,8 @@ chat.use(bodyParser({
         text: ["text/xml"]
     }
 }))
+chat.use(wechat)
 
-chat.use(async (context: Context, next) => {
-    const requestType = getRequestType(context.request.url)
-    switch (requestType) {
-        case "wechat": {
-            const isVerificationMode = context.request.query["echostr"]
-            if (isVerificationMode) {
-                context.body = getWechatVerificationResponseBody(context.request.query)
-            }
-            else {
-                // const responseText = JSON.stringify(context.request.toJSON(), null, 4)
-                const data = await parseXml<WechatNormalTextData>(context.request.body, true)
-                const responseText = data.Content
-                context.body = `
-                    <xml>
-                        <ToUserName><![CDATA[${data.FromUserName}]]></ToUserName>
-                        <FromUserName><![CDATA[${data.ToUserName}]]></FromUserName>
-                        <CreateTime>${Date.now().toString().slice(0, 8)}</CreateTime>
-                        <MsgType><![CDATA[text]]></MsgType>
-                        <Content><![CDATA[${responseText}]]></Content>
-                    </xml>
-                `
-            }
-        }
-    }
-
-    await next
-
+chat.listen(port, () => {
+    console.info("Chat started listening to", port, "at", new Date())
 })
-
-chat.listen(port, () => console.info("Chat started listening to", port, "at", new Date()))
---
+++ src/server/middlewares/wechat.ts
@@ -0,0 +1,78 @@
+import {Context} from "koa"
+
+import {createHash} from "crypto"
+import {parseXml} from "../parseXml"
+
+interface WechatNormalTextData {
+    MsgType: "text"
+    ToUserName: string
+    FromUserName: string
+    CreateTime: string
+    Content: string
+    MsgId: string
+}
+
+const TOKEN = process.env["GOMIGRANT_WECHAT_TOKEN"]
+
+export function sha1(s: any): string {
+    const hasher = createHash("sha1")
+    hasher.update(s)
+    return hasher.digest("hex").toString()
+}
+
+/**
+ * @see https://mp.weixin.qq.com/wiki/8/f9a0b8382e0b77d87b3bcc1ce6fbc104.html
+ */
+function isWechatSignatureValid(
+    token: string,
+    timestamp: string,
+    nonce: string,
+    signature: string,
+): boolean {
+    const hash = sha1([token, timestamp, nonce].sort().join(""))
+    return hash === signature
+}
+
+function getWechatVerificationResponseBody(query: Object): string {
+    const {
+        timestamp,
+        nonce,
+        signature,
+        echostr,
+    } = (query as any)
+    if (isWechatSignatureValid(TOKEN, timestamp, nonce, signature)) {
+        return echostr
+    }
+    else {
+        return ""
+    }
+}
+
+function isWechatRequest(url: string): boolean {
+    return url.includes("api-wechat")
+}
+
+export async function wechat(context: Context, next: () => Promise<any>) {
+    if (!isWechatRequest(context.request.url)) {
+        return next
+    }
+    const isVerificationMode = context.request.query["echostr"]
+    if (isVerificationMode) {
+        context.body = getWechatVerificationResponseBody(context.request.query)
+    }
+    else {
+        // const responseText = JSON.stringify(context.request.toJSON(), null, 4)
+        const data = await parseXml<WechatNormalTextData>(context.request.body, true)
+        const responseText = data.Content
+        context.body = `
+            <xml>
+                <ToUserName><![CDATA[${data.FromUserName}]]></ToUserName>
+                <FromUserName><![CDATA[${data.ToUserName}]]></FromUserName>
+                <CreateTime>${Date.now().toString().slice(0, 8)}</CreateTime>
+                <MsgType><![CDATA[text]]></MsgType>
+                <Content><![CDATA[${responseText}]]></Content>
+            </xml>
+        `
+    }
+    await next
+}
