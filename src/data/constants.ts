95c2cb349f3516d403655d363297b2f4f5b76b88
Andy Shu <andy@symbol.is>
2017-05-18 18:48:39 -0400

    Improve type string declaration
    Signed-off-by: Andy Shu <andyshuhsin@gmail.com>

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,4 +1,4 @@
-export type PATHWAY_KW_SIMPLE = "pathway"
-export const PATHWAY_KW_SIMPLE: PATHWAY_KW_SIMPLE = "pathway"
-export type PATHWAY_KW_COMPOSITE = "pathways"
-export const PATHWAY_KW_COMPOSITE: PATHWAY_KW_COMPOSITE = "pathways"
+export const PATHWAY_KW_SIMPLE = "pathway"
+export type PATHWAY_KW_SIMPLE = typeof PATHWAY_KW_SIMPLE
+export const PATHWAY_KW_COMPOSITE = "pathways"
+export type PATHWAY_KW_COMPOSITE = typeof PATHWAY_KW_COMPOSITE
