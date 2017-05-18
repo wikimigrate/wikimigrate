06858498d4dbe595c2d09a51a03795dfd324cf05
Andy Shu <andy@symbol.is>
2017-05-18 13:39:27 -0400

    Update server-side configurations for SSR
    Signed-off-by: Andy Shu <andyshuhsin@gmail.com>

--- src/server/pm2.config.js
+++ src/server/pm2.config.js
@@ -2,11 +2,13 @@ module.exports = {
     apps : [
         {
             name: 'chat',
+            cwd: './server',
             script: 'server/server/chat.js',
             watch: ['server'],
         },
         {
             name: 'render.bundle',
+            cwd: './web',
             script: 'web/render.bundle.js',
             watch: ['web'],
         },
