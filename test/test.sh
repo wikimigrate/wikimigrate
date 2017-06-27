adaaa60d3a03651c922aa983ac39c2acdc6e06dd
Andy Shu <andy@symbol.is>
2017-06-26 20:16:12 -0400

    Allow testing some files only
    Signed-off-by: Andy Shu <andyshuhsin@gmail.com>

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,13 @@
 cd test
 
-for spec in $(find . -name '*.spec.ts')
+if [ $# -eq 0 ]
+then
+    glob='*.spec.ts'
+else
+    glob="$1*"
+fi
+
+for spec in $(find . -name ${glob})
 do
     echo "\nTesting ${spec}"
     ts-node ${spec}
